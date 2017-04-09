45efb258e027255366c5595b83b098c33ccbd4f4
kroffo <kroffo@oswego.edu>
2017-04-09 13:39:25 -0400

    Positioned the divs better, added scramble functionality.

--- scene.js
+++ scene.js
@@ -1,10 +1,17 @@
 var renderer = new THREE.WebGLRenderer({canvas: document.getElementById("myCanvas"), antialias: true});
 renderer.setClearColor(0x000000);
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth-20, window.innerHeight-20);
+var width = window.innerWidth - 10;
+var height = window.innerHeight - 20 - document.getElementById("settings").offsetHeight;
+renderer.setSize(width, height);
 
+// some variables used for scrambling the cube
+var scrambling = 0;
+var SCRAMBLE_TURNS = 50;
+var previousScrambleTurn = null;
+var oldTurnTime = 0.5;
 
-var fieldOfView = 35;
+var fieldOfView = 50;
 var aspectRatio = window.innerWidth / window.innerHeight;
 
 // How close something can get before it is no longer rendered
@@ -155,43 +162,47 @@ function render() {
 	    positionCamera();
 	}
 
-	// Start turns based on input
-	// Don't start a turn while the cube is already turning
-	if(!cube.turning) {
-	    if(keys[F_KEY] == true) {
-		if(keys[SHIFT_KEY] == true)
-		    cube.startTurn(cube.frontInverse);
-		else
-		    cube.startTurn(cube.front);
-	    } else if(keys[U_KEY] == true) {
-		if(keys[SHIFT_KEY] == true)
-		    cube.startTurn(cube.upInverse);
-		else
-		    cube.startTurn(cube.up);
-	    } else if(keys[R_KEY] == true) {
-		if(keys[SHIFT_KEY] == true)
-		    cube.startTurn(cube.rightInverse);
-		else
-		    cube.startTurn(cube.right);
-	    } else if(keys[D_KEY] == true) {
-		if(keys[SHIFT_KEY] == true)
-		    cube.startTurn(cube.downInverse);
-		else
-		    cube.startTurn(cube.down);
-	    } else if(keys[L_KEY] == true) {
-		if(keys[SHIFT_KEY] == true)
-		    cube.startTurn(cube.leftInverse);
-		else
-		    cube.startTurn(cube.left);
-	    } else if(keys[B_KEY] == true) {
-		if(keys[SHIFT_KEY] == true)
-		    cube.startTurn(cube.backInverse);
-		else
-		    cube.startTurn(cube.back);
+	if(!scrambling) {
+	    // Start turns based on input
+	    // Don't start a turn while the cube is already turning, or while the cube is scrambling
+	    if(!cube.turning) {
+		if(keys[F_KEY] == true) {
+		    if(keys[SHIFT_KEY] == true)
+			cube.startTurn(cube.frontInverse);
+		    else
+			cube.startTurn(cube.front);
+		} else if(keys[U_KEY] == true) {
+		    if(keys[SHIFT_KEY] == true)
+			cube.startTurn(cube.upInverse);
+		    else
+			cube.startTurn(cube.up);
+		} else if(keys[R_KEY] == true) {
+		    if(keys[SHIFT_KEY] == true)
+			cube.startTurn(cube.rightInverse);
+		    else
+			cube.startTurn(cube.right);
+		} else if(keys[D_KEY] == true) {
+		    if(keys[SHIFT_KEY] == true)
+			cube.startTurn(cube.downInverse);
+		    else
+			cube.startTurn(cube.down);
+		} else if(keys[L_KEY] == true) {
+		    if(keys[SHIFT_KEY] == true)
+			cube.startTurn(cube.leftInverse);
+		    else
+			cube.startTurn(cube.left);
+		} else if(keys[B_KEY] == true) {
+		    if(keys[SHIFT_KEY] == true)
+			cube.startTurn(cube.backInverse);
+		    else
+			cube.startTurn(cube.back);
+		}
+	    } else { // Cube is turning
+		// Update the animation of the turn for this render cycle
+		cube.continueTurn();
 	    }
-	} else { // Cube is turning
-	    // Update the animation of the turn for this render cycle
-	    cube.continueTurn();
+	} else { // Scrambling cube
+	    continueScramble();
 	}
     }
 
@@ -209,21 +220,166 @@ function positionCamera() {
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 }
 
+// Update the camera speed based on the camera speed slider
 function updateCameraSpeed() {
     CAMERA_SPEED = document.getElementById("cameraSpeedSlider").value/1000;
 }
+// Add an event listener to call updateCameraSpeed when the slider is updated
 document.getElementById("cameraSpeedSlider").addEventListener("input", updateCameraSpeed, false);
 
+// Update the turn speed based on the camera speed slider
 function updateTurnSpeed() {
-    var time = document.getElementById("turnSpeedSlider").value/1000;
+    var time = (1000 - document.getElementById("turnSpeedSlider").value)/1000;
     cube.updateTurnTime(time);
 }
+// Add an event listener to call updateTurnSpeed when the slider is updated
 document.getElementById("turnSpeedSlider").addEventListener("input", updateTurnSpeed, false);
 
+// Set whether face labels are displayed based on the checkbox
 function toggleFaceLabels() {
     if(document.getElementById("labelsCheckbox").checked)
 	cube.showFaceLabels();
     else
 	cube.hideFaceLabels();
 }
+// Add an event listener to call toggleFaceLabels when the checkbox is clicked
 document.getElementById("labelsCheckbox").addEventListener("click", toggleFaceLabels, false);
+
+// Start scrambling the cube
+// scrambling will keep track of how many turns have been done
+// When scrambling is 0, other inputs will not be processed
+function scramble() {
+    scrambling = 1;
+    disableInputs();
+    oldTurnTime = cube.MAX_TURN_TIME;
+    cube.updateTurnTime(0.1);
+}
+document.getElementById("scrambleButton").addEventListener("click", scramble, false);
+
+function disableInputs() {
+    document.getElementById("cameraSpeedSlider").disabled = true;
+    document.getElementById("turnSpeedSlider").disabled = true;
+    document.getElementById("labelsCheckbox").disabled = true;
+    document.getElementById("scrambleButton").disabled = true;
+}
+
+function enableInputs() {
+    document.getElementById("cameraSpeedSlider").disabled = false;
+    document.getElementById("turnSpeedSlider").disabled = false;
+    document.getElementById("labelsCheckbox").disabled = false;
+    document.getElementById("scrambleButton").disabled = false;
+}
+
+function continueScramble() {
+    if(!cube.turning) {
+	if( scrambling++ >= SCRAMBLE_TURNS) {
+	    scrambling = 0;
+	    enableInputs();
+	    cube.updateTurnTime(oldTurnTime);
+	    return;
+	}
+	switch(Math.floor(Math.random()*12)) {
+	case 0: // Turn the front face
+	    if(previousScrambleTurn === cube.frontInverse)
+		scramble--;
+	    else {
+		previousScrambleTurn = cube.front;
+		cube.startTurn(cube.front);
+	    }
+	    break;
+	case 1: // Turn the up face
+	    if(previousScrambleTurn === cube.upInverse)
+		scramble--;
+	    else {
+		previousScrambleTurn = cube.up;
+		cube.startTurn(cube.up);
+	    }
+	    break;
+	case 2: // Turn the right face
+	    if(previousScrambleTurn === cube.rightInverse)
+		scramble--;
+	    else {
+		previousScrambleTurn = cube.right;
+		cube.startTurn(cube.right);
+	    }
+	    break;
+	case 3: // Turn the down face
+	    if(previousScrambleTurn === cube.downInverse)
+		scramble--;
+	    else {
+		previousScrambleTurn = cube.down;
+		cube.startTurn(cube.down);
+	    }
+	    break;
+	case 4: // Turn the left face
+	    if(previousScrambleTurn === cube.leftInverse)
+		scramble--;
+	    else {
+		previousScrambleTurn = cube.left;
+		cube.startTurn(cube.left);
+	    }
+	    break;
+	case 5: // Turn the back face
+	    if(previousScrambleTurn === cube.backInverse)
+		scramble--;
+	    else {
+		previousScrambleTurn = cube.back;
+		cube.startTurn(cube.back);
+	    }
+	    break;
+	case 6: // Turn the front face reversed
+	    if(previousScrambleTurn === cube.front)
+		scramble--;
+	    else {
+		previousScrambleTurn = cube.frontInverse;
+		cube.startTurn(cube.frontInverse);
+	    }
+	    break;
+	case 7: // Turn the up face reversed
+	    if(previousScrambleTurn === cube.up)
+		scramble--;
+	    else {
+		previousScrambleTurn = cube.upInverse;
+		cube.startTurn(cube.upInverse);
+	    }
+	    break;
+	case 8: // Turn the right face reversed
+	    if(previousScrambleTurn === cube.right)
+		scramble--;
+	    else {
+		previousScrambleTurn = cube.rightInverse;
+		cube.startTurn(cube.rightInverse);
+	    }
+	    break;
+	case 9: // Turn the down face reversed
+	    if(previousScrambleTurn === cube.down)
+		scramble--;
+	    else {
+		previousScrambleTurn = cube.downInverse;
+		cube.startTurn(cube.downInverse);
+	    }
+	    break;
+	case 10: // Turn the left face reversed
+	    if(previousScrambleTurn === cube.left)
+		scramble--;
+	    else {
+		previousScrambleTurn = cube.leftInverse;
+		cube.startTurn(cube.leftInverse);
+	    }
+	    break;
+	case 11: // Turn the back face reversed
+	    if(previousScrambleTurn === cube.back)
+		scramble--;
+	    else {
+		previousScrambleTurn = cube.backInverse;
+		cube.startTurn(cube.backInverse);
+	    }
+	    break;
+	default:
+	    scramble--;
+	}
+    } else {
+	cube.continueTurn();
+    }
+    
+}
